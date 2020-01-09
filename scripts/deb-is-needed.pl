8799bcb409b38b08bb5f137ba72d22be4940e7e1
Wouter Verhelst <wouter.verhelst@zetes.com>
2020-01-09 15:26:53 +0200

    Don't assume that the codename in the .changes file matches any known codename

--- scripts/deb-is-needed.pl
+++ scripts/deb-is-needed.pl
@@ -11,28 +11,38 @@ if(!exists($ENV{REPREPRO_BASE_DIR})) {
 	$ENV{REPREPRO_BASE_DIR} = '/srv/repo/reprepro';
 }
 
-my $found = 0;
+my @codes = (
+	$ENV{DEBIAN_OLDSTABLE_CODE},
+	$ENV{DEBIAN_STABLE_CODE},
+	$ENV{UBUNTU_OLDLTS_CODE},
+	$ENV{UBUNTU_LTS_CODE},
+	$ENV{UBUNTU_STABLE_CODE},
+	$ENV{MINT_OLDLTS_CODE},
+	$ENV{MINT_LTS_CODE},
+);
 
 foreach my $file(@ARGV) {
 	$chg->load($file);
 	my $version = $chg->{Version};
-	my $dist = $chg->{Distribution};
-	$dist =~ s,-,/,g;
 	my @arch = split(/ /, $chg->{Architecture});
 	my $source = $chg->{Source};
 
 	foreach my $arch(@arch) {
-		open my $reprepro, "/usr/bin/reprepro -A $arch list $dist $source|";
-		while(<$reprepro>) {
-			$found = 1;
-			chomp;
-			(undef, undef, my $repovers) = split(/ /);
-			if($version ne $repovers) {
-				exit 0;
+		foreach my $code(@codes) {
+			next unless defined($code);
+			foreach my $prefix("candidate/", "continuous/", "") {
+				open my $reprepro, "/usr/bin/reprepro -A $arch list $prefix$code $source|";
+				while(<$reprepro>) {
+					chomp;
+					(undef, undef, my $repovers) = split(/ /);
+					if($version eq $repovers) {
+						exit 1;
+					}
+				}
+				close $reprepro;
 			}
 		}
 	}
 }
 
-exit 1 if $found;
 exit 0;
